6bbf2395c2a90db9fa86ad86a3218ce25005065d
Matthieu Estrada <m.estrada@alpi.fr>
2016-06-06 11:56:53 +0200

    Remove GLOBAL access to attributes

--- projectfiles.py
+++ projectfiles.py
@@ -13,39 +13,34 @@ class ProjectFiles(object):
 
     # TODO : put this method in projectvariables file
     def write_variables(self):
-        global c_folder_nb
-        global h_folder_nb
-
         # Cpp Dir
         known_cpp = []
-        c_folder_nb = 1
+        ProjectFiles.c_folder_nb = 1
+        self.cmake.write('# Folders files\n')
         for cpp in self.cppfiles:
             if cpp.get('Include') is not None:
                 cxx = str(cpp.get('Include'))
                 current_cpp = '/'.join(cxx.split('\\')[0:-1])
                 if current_cpp not in known_cpp:
                     known_cpp.append(current_cpp)
-                    self.cmake.write('set(CPP_DIR_' + str(c_folder_nb) + ' ' + current_cpp + ')\n')
-                    c_folder_nb += 1
+                    self.cmake.write('set(CPP_DIR_' + str(ProjectFiles.c_folder_nb) + ' ' + current_cpp + ')\n')
+                    ProjectFiles.c_folder_nb += 1
 
         # Headers Dir
         known_headers = []
-        h_folder_nb = 1
+        ProjectFiles.h_folder_nb = 1
         for header in self.headerfiles:
             h = str(header.get('Include'))
             current_header = '/'.join(h.split('\\')[0:-1])
             if current_header not in known_headers:
                 known_headers.append(current_header)
-                self.cmake.write('set(HEADER_DIR_' + str(h_folder_nb) + ' ' + current_header + ')\n')
-                h_folder_nb += 1
+                self.cmake.write('set(HEADER_DIR_' + str(ProjectFiles.h_folder_nb) + ' ' + current_header + ')\n')
+                ProjectFiles.h_folder_nb += 1
 
     def write_files(self):
         """
         Add directory variables to SRC_FILES
         """
-        global c_folder_nb
-        global h_folder_nb
-
         # Add files to project
         # TODO Glob Recurse for files.
         self.cmake.write('################ Files ################\n'
@@ -53,11 +48,11 @@ class ProjectFiles(object):
                          '#######################################\n\n')
         self.cmake.write('file(GLOB SRC_FILES\n')
         c = 1
-        while c < c_folder_nb:
+        while c < ProjectFiles.c_folder_nb:
             self.cmake.write('    ${CPP_DIR_' + str(c) + '}/*.cpp\n')
             c += 1
         h = 1
-        while h < h_folder_nb:
+        while h < ProjectFiles.h_folder_nb:
             self.cmake.write('    ${HEADER_DIR_' + str(h) + '}/*.h\n')
             h += 1
         self.cmake.write(')\n\n')
