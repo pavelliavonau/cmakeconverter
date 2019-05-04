9789e505b41f681c6fcd54ecd9672bd1db4be170
Pavel Liavonau <liavonlida@gmail.com>
2019-05-04 17:30:19 +0300

    vcxproj: do not ignore WIN32 define

--- cmake_converter/visual_studio/vcxproj/flags.py
+++ cmake_converter/visual_studio/vcxproj/flags.py
@@ -141,7 +141,7 @@ class CPPFlags(Flags):
 
         """
         for define in defines_node.text.split(";"):
-            if define not in ('%(PreprocessorDefinitions)', 'WIN32'):
+            if define != '%(PreprocessorDefinitions)':
                 define = replace_vs_vars_with_cmake_vars(context, define)
                 define = define.replace('\\', '\\\\')
                 define = define.replace('"', '\\"')
