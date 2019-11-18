0da9e8f31ff658d7bbc9d09ffa90028c37e2f144
Pavel Liavonau <liavonlida@gmail.com>
2019-11-18 16:44:11 +0300

    common: updated year 2018 -> 2019.

--- cmake_converter/visual_studio/vcxproj/context_initializer.py
+++ cmake_converter/visual_studio/vcxproj/context_initializer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2016-2018:
+# Copyright (c) 2016-2019:
 #   Matthieu Estrada, ttamalfor@gmail.com
 #   Pavel Liavonau, liavonlida@gmail.com
 #
