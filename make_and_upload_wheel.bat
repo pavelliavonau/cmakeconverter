3c8af4869a005e5fea507fb81de041f5432ef931
Pavel Liavonau <liavonlida@gmail.com>
2019-12-10 14:53:55 +0300

    pypi: fixed url for uploading to test pypi

--- make_and_upload_wheel.bat
+++ make_and_upload_wheel.bat
@@ -1,7 +1,7 @@
 python setup.py sdist bdist_wheel
 
 rem test uploading
-twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 
 rem release uploading
 rem twine upload dist/*
